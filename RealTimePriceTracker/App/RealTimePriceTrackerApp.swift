6ad38149118105d4dd2de0a0e1c6a2ee72b03e99
Paurush Gupta <paurushgupta@Paurushs-MacBook-Air.local>
2026-03-09 11:15:07 +0530

    project setup

---
+++ RealTimePriceTracker/App/RealTimePriceTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealTimePriceTrackerApp.swift
+//  RealTimePriceTracker
+//
+//  Created by Paurush Gupta on 09/03/26.
+//
+
+import SwiftUI
+
+@main
+struct RealTimePriceTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
